709c3cf4f0a5c7d4679e19de07f6328bde8e8aea
Abd Allah Zidan <eng.abdallahzidan@gmail.com>
2020-01-23 13:08:12 +0200

    project structure

---
+++ src/tictac/game/MainGame.java
@@ -0,0 +1,21 @@
+
+package tictac.game;
+//import tictac.ui.*;
+// import tictac.network.*;
+// import tictac.database.*;
+import javafx.application.Application;
+
+import javafx.stage.Stage;
+
+public class MainGame extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+       
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
