7f4f7b96f09ff7f33cb904868a98f6b5e1804f3f
alhaitham <alhaithamkamalali@gmail.com>
2020-01-24 15:05:59 +0200

    initial database connection

--- src/tictac/database/Sample.java
+++ src/tictac/database/Sample.java
@@ -5,10 +5,38 @@
  */
 package tictac.database;
 
-/**
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/** 
  *
  * @author zidan
  */
 public class Sample {
-    
+    public static void main(String[] args) {
+        connect();
+    }
+    public static void connect(){
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:TTTDB.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            
+            System.out.println("Connection to SQLite has been established.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
 }
