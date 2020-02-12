e4a02f3f31fd947d342a8072133b97f150eb6354
alhaitham <alhaithamkamalali@gmail.com>
2020-02-12 14:34:20 +0200

    add create and drop db

--- src/tictac/database/DBConnection.java
+++ src/tictac/database/DBConnection.java
@@ -5,9 +5,11 @@
  */
 package tictac.database;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /** 
  *
@@ -17,12 +19,18 @@ public class DBConnection {
     private Connection conn;
 
     public Connection connect(){
-        try {
-            String url = "jdbc:sqlite:TTTDB";
-            conn = DriverManager.getConnection(url);            
-            //System.out.println("Connection to SQLite has been established.");
-        } catch (SQLException e) {
-            System.out.println("Faild to connect to database\n"+e.getMessage());
+        File file = new File ("TTTDB");
+        if(file.exists()){
+            try {
+                String url = "jdbc:sqlite:TTTDB";
+                conn = DriverManager.getConnection(url);            
+                //System.out.println("Connection to SQLite has been established.");
+            } catch (SQLException e) {
+                System.out.println("Faild to connect to database\n"+e.getMessage());
+            }
+        }
+        else {
+            conn = createDB();
         }
         return conn;
     }
@@ -33,5 +41,82 @@ public class DBConnection {
         } catch (SQLException ex) {
             System.out.println("Faild to close connection with database\n"+ex.getMessage());
         }
+    }
+    public Connection createDB(){
+        try {
+            String url = "jdbc:sqlite:TTTDB";
+            conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            String userTable = "CREATE TABLE \"users\" (\n" +
+                    "	\"id\"	INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                    "	\"username\"	TEXT NOT NULL UNIQUE,\n" +
+                    "	\"password\"	TEXT NOT NULL UNIQUE,\n" +
+                    "	\"fname\"	TEXT DEFAULT 'user',\n" +
+                    "	\"lname\"	TEXT,\n" +
+                    "	\"score\"	INTEGER DEFAULT 0\n" +
+                    ");";
+            String playerTable = "CREATE TABLE \"players\" (\n" +
+                    "	\"id\"	INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                    "	\"fname\"	TEXT DEFAULT 'Oponent',\n" +
+                    "	\"lname\"	TEXT,\n" +
+                    "	\"ip_address\"	TEXT UNIQUE,\n" +
+                    "	\"user_id\"	INTEGER,\n" +
+                    "	FOREIGN KEY(\"user_id\") REFERENCES \"users\"(\"id\")\n" +
+                    ");";
+            String gameTable = "CREATE TABLE \"games\" (\n" +
+                    "	\"id\"	INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                    "	\"game_type\"	TEXT NOT NULL CHECK(game_type IN ('solo','dual')),\n" +
+                    "	\"player_id\"	INTEGER,\n" +
+                    "	\"result\"	TEXT NOT NULL CHECK(result IN ('victory','loss','draw')),\n" +
+                    "	\"sympol\"	TEXT NOT NULL CHECK(sympol IN ('x','o')),\n" +
+                    "	\"user_id\"	INTEGER,\n" +
+                    "	\"level\"	TEXT,\n" +
+                    "	\"timestamp\"	TEXT DEFAULT CURRENT_TIMESTAMP,\n" +
+                    "	FOREIGN KEY(\"player_id\") REFERENCES \"players\"(\"id\")\n" +
+                    ");";
+            String stepTable = "CREATE TABLE \"steps\" (\n" +
+                    "	\"id\"	INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+                    "	\"x\"	INTEGER NOT NULL,\n" +
+                    "	\"y\"	INTEGER NOT NULL,\n" +
+                    "	\"game_id\"	INTEGER NOT NULL,\n" +
+                    "	\"turn\"	TEXT NOT NULL CHECK(turn IN ('mine','oponent')),\n" +
+                    "	\"step_no\"	INTEGER,\n" +
+                    "	FOREIGN KEY(\"game_id\") REFERENCES \"games\"(\"id\")\n" +
+                    ");";
+            stmt.execute(userTable);
+            stmt.execute(playerTable);
+            stmt.execute(gameTable);
+            stmt.execute(stepTable);
+        }
+        catch (SQLException ex) {
+            System.out.println("Faild to close create database\n"+ex.getMessage());
+        }
+        return conn;
+    }
+    public void dropDB(){
+        try {
+            String url = "jdbc:sqlite:TTTDB";
+            conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            String dropUsers = "DELETE FROM users;";
+            String dropPlayers = "DELETE FROM players;";
+            String dropGames = "DELETE FROM games;";
+            String dropSteps = "DELETE FROM steps;";
+            String dropSeq = "DELETE FROM sqlite_sequence;";
+
+            stmt.execute(dropUsers);
+            stmt.execute(dropPlayers);
+            stmt.execute(dropGames);
+            stmt.execute(dropSteps);
+            stmt.execute(dropSeq);
+            conn.close();
+        }
+        catch(SQLException ex){
+            System.out.println("Faild to drop create database\n"+ex.getMessage());
+        }
+    }
+    public static void main(String[] args) {
+        DBConnection db = new DBConnection();
+        db.dropDB();
     }
 }
